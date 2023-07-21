7a906894431856a4391a1c756edd0cfd85da5c9d
nisalasanga <nisalasanga@gmail.com>
2023-07-21 22:43:06 +0530

    After Bug which not moing track Id

--- object_detection and main.py
+++ object_detection and main.py
@@ -13,7 +13,6 @@ cap = cv2.VideoCapture(video_path)
 tracker = Tracker(150, 30, 5)
 
 
-
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -25,6 +24,7 @@ while cap.isOpened():
 
         boxes = result[0].boxes.cpu().numpy()                                  # get boxes on cpu in numpy
         centers = []
+        label_cordinates = []
 
         
         for j in range(len(boxes)):
@@ -34,24 +34,28 @@ while cap.isOpened():
                 cy = int((y1 + y2)/2)                                       # Mid point of x 
                 #cv2.circle(frame,(cx, cy), 5, (0, 0, 255),-1)               # Mid point Drawing
                 centers.append([cx,cy])
+                label_cordinates.append([x1,y2])
                 cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)        # draw boxes on img
  
         centers = np.array(centers)
-        tracker.update(centers)
+        tracker.update(centers,label_cordinates)
+        print(centers)
+        print(len(tracker.tracks))
 
         for j in range(len(tracker.tracks)):
             if (len(tracker.tracks[j].trace) > 1):
-                x = int(tracker.tracks[j].trace[-1][0,0]) 
-                y = int(tracker.tracks[j].trace[-1][0,1])
+                #x = int(tracker.tracks[j].trace[-1][0,0]) 
+                #y = int(tracker.tracks[j].trace[-1][0,1])
+                #print(tracker.tracks[j].trace[-1])
 
-                tl = (x-10,y-10)
-                br = (x+10,y+10)
-                cv2.rectangle(frame,tl,br,(0, 255, 0),1)
-                cv2.putText(frame,str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, (0,255,250),2)
-                for k in range(len(tracker.tracks[j].trace)):
+                #tl = (x-10,y-10)
+                #br = (x+10,y+10)
+                #cv2.rectangle(frame,tl,br,(255, 0, 0),1)
+                cv2.putText(frame,str(tracker.tracks[j].trackId), (tracker.tracks[j].label[0],tracker.tracks[j].label[1]),0, 0.5, (0,255,250),2)
+                '''for k in range(len(tracker.tracks[j].trace)):
                     x = int(tracker.tracks[j].trace[k][0,0])
                     y = int(tracker.tracks[j].trace[k][0,1])
-                    cv2.circle(frame,(x,y), 3, (255, 0, 0),-1)
+                    cv2.circle(frame,(x,y), 3, (255, 0, 0),-1)'''
 
             #cv2.circle(frame,(int(data[j,i,0]),int(data[j,i,1])), 6, (0,0,0),-1)
                  
@@ -59,7 +63,7 @@ while cap.isOpened():
         cv2.imshow("YOLOv8 Inference", frame)
 
         # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        if cv2.waitKey(0) & 0xFF == ord("q"):
             break
     else:
         # Break the loop if the end of the video is reached
